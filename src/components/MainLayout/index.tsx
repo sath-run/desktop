8e0d338cab990488d183a2b729542617f00b9c80
wangjb2 <wangjb2@xiaopeng.com>
2022-09-29 15:17:49 +0800

    feat: 切换chakra-ui

--- src/components/MainLayout/index.tsx
+++ src/components/MainLayout/index.tsx
@@ -5,7 +5,7 @@ const MainLayout = () => {
     const outlet = useOutlet();
     return (
         <Box pt={'40px'}>
-            <Center style={{appRegion: 'drag'}} height={'40px'} position={'fixed'} fontSize={16} color={'whiteAlpha.900'} fontWeight={'700'} top={0} left={0} right={0} backgroundColor={'brand.500'}>
+            <Center className={'page-header'} height={'40px'} position={'fixed'} fontSize={16} color={'whiteAlpha.900'} fontWeight={'700'} top={0} left={0} right={0} backgroundColor={'brand.500'}>
                 SATH
             </Center>
             {outlet}
